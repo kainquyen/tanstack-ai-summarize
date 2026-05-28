982358acfdf8775c2bedd78ed38ddaef75bc19eb
Nguyễn Phú Khải <nkhai648@gmail.com>
2026-05-28 14:11:53 +0700

    validator search param items

--- src/routes/dashboard/items/index.tsx
+++ src/routes/dashboard/items/index.tsx
@@ -1,55 +1,110 @@
 import { Badge } from '#/components/ui/badge'
 import { Button } from '#/components/ui/button'
 import { Card, CardHeader, CardTitle } from '#/components/ui/card'
+import { Input } from '#/components/ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '#/components/ui/select'
 import { getItemsFn } from '#/data/items'
+import { ItemStatus } from '#/generated/prisma/enums'
 import { copyToClipboard } from '#/lib/clipboard'
 import { createFileRoute, Link } from '@tanstack/react-router'
 import { Copy } from 'lucide-react'
+import z from 'zod'
+
+const itemsSearchSchema = z.object({
+  q: z.string().default(''),
+  filter: z.union([z.nativeEnum(ItemStatus), z.literal('all')]).catch('all')
+})
 
 export const Route = createFileRoute('/dashboard/items/')({
   component: RouteComponent,
   loader: () => getItemsFn(),
+  validateSearch: itemsSearchSchema
 })
 
 function RouteComponent() {
   const data = Route.useLoaderData()
   return (
-    <div className="grid gap-6 md:grid-cols-2">
-      {data.map((item) => (
-        <Card
-          key={item.id}
-          className="group overflow-hidden transition-all hover:shadow-lg pt-0"
-        >
-          <Link to="/dashboard" className="block">
-            {item.ogImage && (
-              <div className="aspect-video w-full overflow-hidden bg-muted">
-                <img
-                  src={item.ogImage}
-                  alt={item.title ?? 'Article image'}
-                  className="h-full w-full object-cover transition-transform group-hover:scale-105"
-                />
-              </div>
-            )}
-            <CardHeader className='space-y-3 pt-4'> 
-              <div className="flex items-center justify-between gap-2">
-                <Badge variant={item.status === 'COMPLETED' ? 'default' : 'secondary'}>{item.status.toLowerCase()}</Badge>
-                <Button className="size-8" variant="outline" size="icon" onClick={async(e) => {
-                  e.preventDefault()
-                  await copyToClipboard(item.url)
-                }}>
-                  <Copy className="size-4"/>
-                </Button>
-              </div>
+    <div className="flex flex-1 flex-col gap-6">
+      <div>
+        <h1 className="text-2xl font-bold">Saved Items</h1>
+        <p className="text-muted-foreground">
+          Your saved articles and content!
+        </p>
+      </div>
 
-              <CardTitle className="line-clamp-1 text-xl leading-snug group-hover:text-primary transition-colors">
-                {item.title}
-              </CardTitle>
+      <div className="flex gap-4">
+        <Input placeholder="Search by title or tags" />
+        <Select>
+          <SelectTrigger className="w-[200px]">
+            <SelectValue placeholder="Filter by status" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="all">All Statutes</SelectItem>
+            {Object.values(ItemStatus).map((status) => (
+              <SelectItem key={status} value={status}>
+                {status.charAt(0) + status.slice(1).toLowerCase()}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
 
-              {item.author && <p className="text-muted-foreground text-xs">{item.author}</p>}
-            </CardHeader>
-          </Link>
-        </Card>
-      ))}
+      <div className="grid gap-6 md:grid-cols-2">
+        {data.map((item) => (
+          <Card
+            key={item.id}
+            className="group overflow-hidden transition-all hover:shadow-lg pt-0"
+          >
+            <Link to="/dashboard" className="block">
+              {item.ogImage && (
+                <div className="aspect-video w-full overflow-hidden bg-muted">
+                  <img
+                    src={item.ogImage}
+                    alt={item.title ?? 'Article image'}
+                    className="h-full w-full object-cover transition-transform group-hover:scale-105"
+                  />
+                </div>
+              )}
+              <CardHeader className="space-y-3 pt-4">
+                <div className="flex items-center justify-between gap-2">
+                  <Badge
+                    variant={
+                      item.status === 'COMPLETED' ? 'default' : 'secondary'
+                    }
+                  >
+                    {item.status.toLowerCase()}
+                  </Badge>
+                  <Button
+                    className="size-8"
+                    variant="outline"
+                    size="icon"
+                    onClick={async (e) => {
+                      e.preventDefault()
+                      await copyToClipboard(item.url)
+                    }}
+                  >
+                    <Copy className="size-4" />
+                  </Button>
+                </div>
+
+                <CardTitle className="line-clamp-1 text-xl leading-snug group-hover:text-primary transition-colors">
+                  {item.title}
+                </CardTitle>
+
+                {item.author && (
+                  <p className="text-muted-foreground text-xs">{item.author}</p>
+                )}
+              </CardHeader>
+            </Link>
+          </Card>
+        ))}
+      </div>
     </div>
   )
 }
